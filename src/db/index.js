4b7078f046950723c899c4282834b4d8bd69fd8a
tushar99119911 <tushar.22scse1012576@galgotiasuniversity.edu.in>
2026-09-18 16:38:05 +0530

    Fix backend database startup

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async() =>{
+    try{
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    }catch(error){
+        console.log("MOngoDB connection error: ", error);
+        process.exit(1);
+    }
+
+}
+
+export default connectDB;
